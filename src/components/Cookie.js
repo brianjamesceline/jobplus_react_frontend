6a221d08ab2c6f6dac93ba01dc1f68c50bc8fd51
Brian Celine <brianceline@gmail.com>
2022-01-15 00:28:53 +0000

    Cookie edit

--- src/components/Cookie.js
+++ src/components/Cookie.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import { AppBar, Collapse } from '@material-ui/core'
+import { Alert } from '@material-ui/lab';
+import React, { useState } from 'react'
 
 export default function Cookie() {
+  const [open, setOpen] = useState(true);
+  
   return (
-    <div>
-      
-    </div>
+    <AppBar>
+      <Collapse in={open}>
+        <Alert>
+          JobPlus uses cookies. By continuing you are agreeing to our use of cookies.
+        </Alert>
+      </Collapse>
+    </AppBar>
   )
 }
