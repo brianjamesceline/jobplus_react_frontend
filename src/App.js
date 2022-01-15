e965a708b51fd52d762887e062e6775ad365fc0e
Brian Celine <brianceline@gmail.com>
2022-01-15 00:01:09 +0000

    Cookie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, useHistory } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() { 
   const history = useHistory();
