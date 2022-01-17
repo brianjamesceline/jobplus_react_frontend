4444ace9be871fb473c088c45bbad9dba979aadf
Brian Celine <brianceline@gmail.com>
2022-01-17 22:42:19 +0000

    FormControls

--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -1,10 +1,14 @@
 import ButtonCustom from './ButtonCustom';
 import FormGroupCustom from './FormGroupCustom';
+import InputLabelCustom from './InputLabelCustom';
+import TextFieldCustom from './TextFieldCustom';
 
 
 const Controls = {
   ButtonCustom,
   FormGroupCustom,
+  InputLabelCustom,
+  TextFieldCustom,
 }
 
 export default Controls;
