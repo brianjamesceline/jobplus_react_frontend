d56e13581c15d7d289237501350a1757a637cc40
Brian Celine <brianceline@gmail.com>
2022-01-17 22:18:48 +0000

    FormControls

---
+++ src/components/controls/Controls.js
@@ -0,0 +1,10 @@
+import ButtonCustom from './ButtonCustom';
+import FormGroupCustom from './FormGroupCustom';
+
+
+const Controls = {
+  ButtonCustom,
+  FormGroupCustom,
+}
+
+export default Controls;
