9a8f66eb5f3b7bae36df7f1f10069d31a8c094f4
Brian Celine <brianceline@gmail.com>
2022-01-17 17:10:27 +0000

    PageSetup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
-import { BrowserRouter, useHistory } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Cookie from './components/Cookie';
+import Home from './pages/Home';
 
 function App() { 
   let history = useHistory();
   return (
-    
+    <>
       <BrowserRouter history={history}>
         <Navbar />
+        <Switch>
+          <Route exact path='/' render={props => <Home {...props} />} />
+        </Switch>
         <Cookie />
         <Footer />
       </BrowserRouter>
-    
-  )
+    </>
+  );
 }
 
 export default App;
