05947aa8e6b17b3cad6c191658a6b82e87ad46be
Brian Celine <brianceline@gmail.com>
2022-01-19 15:35:29 +0000

    Sector

--- src/theme.js
+++ src/theme.js
@@ -57,9 +57,12 @@ export default createTheme({
   container: {
     width: '900px',
     margin: '0 auto',
-    // padding: '0 2rem',
+    padding: '0',
     [breakpoints.down('sm')]: {
       width: '100%',
+    },
+    [breakpoints.down('md')]: {
+      padding: '0 20px',
     },
   }
 });
